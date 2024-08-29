2c0028c6134f6d17b36aa18f2667b2e5592bec68
CarolusRex <bolverk.damon@gmail.com>
2024-08-29 14:08:40 +0200

    1

--- script.sh
+++ script.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 echo "entrer l'emplacement:" 
-read destination
+read -p destination
 cd "$destination"
 mkdir mon_dossier
 cd mon_dossier
+if [ ! -f collection.txt ]; then
 touch collection.txt
-echo "entrer le nom d'un film:"
-read film
+fi
+firead -p "Voulez vous ajouter le film? (y/n) " -n 1 -r; echo; 
+if [[ $REPLY =~ ^[Yy]$ ]]; then read film
 echo "$film" >> collection.txt
+fi
